1383f3a8ffda9d96e187e0ddb8d374339690419e
Hugo JAUBERT <hjaubert7@gmail.com>
2024-11-12 15:25:47 +0100

    changement BDD

--- SQL/BDD.sql
+++ SQL/BDD.sql
@@ -1,139 +1,160 @@
--- Création de la table Class (Classe des personnages)
-CREATE TABLE Class (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT,
-    base_pv INT NOT NULL,
-    base_mana INT NOT NULL,
-    strength INT NOT NULL,
-    initiative INT NOT NULL,
-    max_items INT NOT NULL
+CREATE TABLE `Class` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `description` TEXT,
+    `base_pv` INT NOT NULL,
+    `base_mana` INT NOT NULL,
+    `strength` INT NOT NULL,
+    `initiative` INT NOT NULL,
+    `max_items` INT NOT NULL
 );
 
--- Création de la table Items (Objets disponibles dans le jeu)
-CREATE TABLE Items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT
+CREATE TABLE `Items` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `description` TEXT
 );
 
--- Création de la table Loot (Butins des monstres)
-CREATE TABLE Loot (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    item_id INT, -- Relation avec Items
-    quantity INT NOT NULL,
-    FOREIGN KEY (item_id) REFERENCES Items(id)
+CREATE TABLE `Loot` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `item_id` INT,
+    `quantity` INT NOT NULL
 );
 
--- Création de la table Treasure (Trésors dans les chapitres)
-CREATE TABLE Treasure (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    item_id INT, -- Relation avec Items
-    quantity INT NOT NULL,
-    FOREIGN KEY (item_id) REFERENCES Items(id)
+CREATE TABLE `Treasure` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `item_id` INT,
+    `quantity` INT NOT NULL
 );
 
--- Création de la table Monster (Monstres rencontrés dans l'histoire)
-CREATE TABLE Monster (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    pv INT NOT NULL,
-    mana INT,
-    initiative INT NOT NULL,
-    strength INT NOT NULL,
-    attack TEXT,
-    loot_id INT, -- Relation avec Loot
-    xp INT NOT NULL,
-    FOREIGN KEY (loot_id) REFERENCES Loot(id)
+CREATE TABLE `Monster` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `pv` INT NOT NULL,
+    `mana` INT,
+    `initiative` INT NOT NULL,
+    `strength` INT NOT NULL,
+    `attack` TEXT,
+    `loot_id` INT,
+    `xp` INT NOT NULL
 );
 
--- Création de la table Hero (Personnage principal)
-CREATE TABLE Hero (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    class_id INT, -- Relation avec Class
-    image VARCHAR(255),
-    biography TEXT,
-    pv INT NOT NULL,
-    mana INT NOT NULL,
-    strength INT NOT NULL,
-    initiative INT NOT NULL,
-    armor VARCHAR(50),
-    primary_weapon VARCHAR(50),
-    secondary_weapon VARCHAR(50),
-    shield VARCHAR(50),
-    spell_list TEXT,
-    xp INT NOT NULL,
-    current_level INT DEFAULT 1,
-    FOREIGN KEY (class_id) REFERENCES Class(id)
+CREATE TABLE `Hero` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `class_id` INT,
+    `user_id` INT,
+    `image` VARCHAR(255),
+    `biography` TEXT,
+    `pv` INT NOT NULL,
+    `mana` INT NOT NULL,
+    `strength` INT NOT NULL,
+    `initiative` INT NOT NULL,
+    `armor` VARCHAR(50),
+    `primary_weapon` VARCHAR(50),
+    `secondary_weapon` VARCHAR(50),
+    `shield` VARCHAR(50),
+    `spell_id` int,
+    `xp` INT NOT NULL,
+    `current_level` INT DEFAULT 1
 );
 
--- Création de la table Level (Niveaux de progression des classes)
-CREATE TABLE Level (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    class_id INT, -- Relation avec Class
-    level INT NOT NULL,
-    required_xp INT NOT NULL,
-    pv_bonus INT NOT NULL,
-    mana_bonus INT NOT NULL,
-    strength_bonus INT NOT NULL,
-    initiative_bonus INT NOT NULL,
-    FOREIGN KEY (class_id) REFERENCES Class(id)
+CREATE TABLE `Level` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `class_id` INT,
+    `level` INT NOT NULL,
+    `required_xp` INT NOT NULL,
+    `pv_bonus` INT NOT NULL,
+    `mana_bonus` INT NOT NULL,
+    `strength_bonus` INT NOT NULL,
+    `initiative_bonus` INT NOT NULL
 );
 
--- Création de la table Chapter (Chapitres de l'histoire)
-CREATE TABLE Chapter (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    content TEXT NOT NULL,
-    image VARCHAR(255),
-    treasure_id INT, -- Relation avec Treasure
-    FOREIGN KEY (treasure_id) REFERENCES Treasure(id)
+CREATE TABLE `Chapter` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `content` TEXT NOT NULL,
+    `image` VARCHAR(255),
+    `treasure_id` INT
 );
 
--- Création de la table Encounter (Rencontres dans les chapitres)
-CREATE TABLE Encounter (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    chapter_id INT,
-    monster_id INT,
-    FOREIGN KEY (chapter_id) REFERENCES Chapter(id),
-    FOREIGN KEY (monster_id) REFERENCES Monster(id)
+CREATE TABLE `Encounter` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `chapter_id` INT,
+    `monster_id` INT
 );
 
--- Table intermédiaire pour l'inventaire des héros (Hero - Items)
-CREATE TABLE Inventory (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    hero_id INT,
-    item_id INT,
-    FOREIGN KEY (hero_id) REFERENCES Hero(id),
-    FOREIGN KEY (item_id) REFERENCES Items(id)
+CREATE TABLE `Inventory` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `hero_id` INT,
+    `item_id` INT
 );
 
--- Création de la table Links (Liens entre chapitres)
-CREATE TABLE Links (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    chapter_id INT,
-    next_chapter_id INT,
-    description TEXT,
-    FOREIGN KEY (chapter_id) REFERENCES Chapter(id),
-    FOREIGN KEY (next_chapter_id) REFERENCES Chapter(id)
+CREATE TABLE `Links` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `chapter_id` INT,
+    `next_chapter_id` INT,
+    `description` TEXT
 );
 
--- Table intermédiaire pour les trésors dans les chapitres (Chapter - Items)
-CREATE TABLE Chapter_Treasure (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    chapter_id INT,
-    item_id INT,
-    FOREIGN KEY (chapter_id) REFERENCES Chapter(id),
-    FOREIGN KEY (item_id) REFERENCES Items(id)
+CREATE TABLE `Chapter_Treasure` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `chapter_id` INT,
+    `item_id` INT
 );
 
--- Table intermédiaire pour les quêtes des héros (Hero - Chapter)
-CREATE TABLE Quest (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    hero_id INT,
-    chapter_id INT,
-    FOREIGN KEY (hero_id) REFERENCES Hero(id),
-    FOREIGN KEY (chapter_id) REFERENCES Chapter(id)
+CREATE TABLE `Quest` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `hero_id` INT,
+    `chapter_id` INT
 );
+
+CREATE TABLE `User` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `nom` text NOT NULL,
+    `mdp` text NOT NULL,
+    `admin` bool NOT NULL
+);
+
+CREATE TABLE `Spell` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `nom` text,
+    `mana` int NOT NULL
+);
+
+ALTER TABLE `Spell` ADD FOREIGN KEY (`id`) REFERENCES `Hero` (`spell_id`);
+
+ALTER TABLE `Hero` ADD FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
+
+ALTER TABLE `Loot` ADD FOREIGN KEY (`item_id`) REFERENCES `Items` (`id`);
+
+ALTER TABLE `Treasure` ADD FOREIGN KEY (`item_id`) REFERENCES `Items` (`id`);
+
+ALTER TABLE `Monster` ADD FOREIGN KEY (`loot_id`) REFERENCES `Loot` (`id`);
+
+ALTER TABLE `Hero` ADD FOREIGN KEY (`class_id`) REFERENCES `Class` (`id`);
+
+ALTER TABLE `Level` ADD FOREIGN KEY (`class_id`) REFERENCES `Class` (`id`);
+
+ALTER TABLE `Chapter` ADD FOREIGN KEY (`treasure_id`) REFERENCES `Treasure` (`id`);
+
+ALTER TABLE `Encounter` ADD FOREIGN KEY (`chapter_id`) REFERENCES `Chapter` (`id`);
+
+ALTER TABLE `Encounter` ADD FOREIGN KEY (`monster_id`) REFERENCES `Monster` (`id`);
+
+ALTER TABLE `Inventory` ADD FOREIGN KEY (`hero_id`) REFERENCES `Hero` (`id`);
+
+ALTER TABLE `Inventory` ADD FOREIGN KEY (`item_id`) REFERENCES `Items` (`id`);
+
+ALTER TABLE `Links` ADD FOREIGN KEY (`chapter_id`) REFERENCES `Chapter` (`id`);
+
+ALTER TABLE `Links` ADD FOREIGN KEY (`next_chapter_id`) REFERENCES `Chapter` (`id`);
+
+ALTER TABLE `Chapter_Treasure` ADD FOREIGN KEY (`chapter_id`) REFERENCES `Chapter` (`id`);
+
+ALTER TABLE `Chapter_Treasure` ADD FOREIGN KEY (`item_id`) REFERENCES `Items` (`id`);
+
+ALTER TABLE `Quest` ADD FOREIGN KEY (`hero_id`) REFERENCES `Hero` (`id`);
+
+ALTER TABLE `Quest` ADD FOREIGN KEY (`chapter_id`) REFERENCES `Chapter` (`id`);
