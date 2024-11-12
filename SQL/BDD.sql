95bf1ae4221aeb13af191d1b9de566b77f194c5f
Hugo JAUBERT <hjaubert7@gmail.com>
2024-11-12 17:53:47 +0100

    ameliorations BDD

--- SQL/BDD.sql
+++ SQL/BDD.sql
@@ -1,129 +1,152 @@
 CREATE TABLE `Class` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50) NOT NULL,
-    `description` TEXT,
-    `base_pv` INT NOT NULL,
-    `base_mana` INT NOT NULL,
-    `strength` INT NOT NULL,
-    `initiative` INT NOT NULL,
-    `max_items` INT NOT NULL
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `description` TEXT,
+  `base_pv` INT NOT NULL,
+  `base_mana` INT NOT NULL,
+  `strength` INT NOT NULL,
+  `initiative` INT NOT NULL,
+  `max_items` INT NOT NULL,
+  `primary_weapon_default` text,
+  `secondary_weapon_default` text
 );
 
 CREATE TABLE `Items` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50) NOT NULL,
-    `description` TEXT
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `description` TEXT
 );
 
 CREATE TABLE `Loot` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50) NOT NULL,
-    `item_id` INT,
-    `quantity` INT NOT NULL
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `item_id` INT,
+  `quantity` INT NOT NULL
 );
 
 CREATE TABLE `Treasure` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50) NOT NULL,
-    `item_id` INT,
-    `quantity` INT NOT NULL
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `item_id` INT,
+  `quantity` INT NOT NULL
 );
 
 CREATE TABLE `Monster` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50) NOT NULL,
-    `pv` INT NOT NULL,
-    `mana` INT,
-    `initiative` INT NOT NULL,
-    `strength` INT NOT NULL,
-    `attack` TEXT,
-    `loot_id` INT,
-    `xp` INT NOT NULL
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `pv` INT NOT NULL,
+  `mana` INT,
+  `initiative` INT NOT NULL,
+  `strength` INT NOT NULL,
+  `attack` TEXT,
+  `loot_id` INT,
+  `xp` INT NOT NULL
 );
 
 CREATE TABLE `Hero` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50) NOT NULL,
-    `class_id` INT,
-    `user_id` INT,
-    `image` VARCHAR(255),
-    `biography` TEXT,
-    `pv` INT NOT NULL,
-    `mana` INT NOT NULL,
-    `strength` INT NOT NULL,
-    `initiative` INT NOT NULL,
-    `armor` VARCHAR(50),
-    `primary_weapon` VARCHAR(50),
-    `secondary_weapon` VARCHAR(50),
-    `shield` VARCHAR(50),
-    `spell_id` int,
-    `xp` INT NOT NULL,
-    `current_level` INT DEFAULT 1
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `class_id` INT,
+  `user_id` INT,
+  `image` VARCHAR(255),
+  `biography` TEXT,
+  `pv` INT NOT NULL,
+  `mana` INT NOT NULL,
+  `strength` INT NOT NULL,
+  `initiative` INT NOT NULL,
+  `armor` VARCHAR(50),
+  `primary_weapon` VARCHAR(50),
+  `secondary_weapon` VARCHAR(50),
+  `shield` VARCHAR(50),
+  `spell_id` int,
+  `xp` INT NOT NULL,
+  `current_level` INT DEFAULT 1
 );
 
 CREATE TABLE `Level` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `class_id` INT,
-    `level` INT NOT NULL,
-    `required_xp` INT NOT NULL,
-    `pv_bonus` INT NOT NULL,
-    `mana_bonus` INT NOT NULL,
-    `strength_bonus` INT NOT NULL,
-    `initiative_bonus` INT NOT NULL
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `class_id` INT,
+  `level` INT NOT NULL,
+  `required_xp` INT NOT NULL,
+  `pv_bonus` INT NOT NULL,
+  `mana_bonus` INT NOT NULL,
+  `strength_bonus` INT NOT NULL,
+  `initiative_bonus` INT NOT NULL
 );
 
 CREATE TABLE `Chapter` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `content` TEXT NOT NULL,
-    `image` VARCHAR(255),
-    `treasure_id` INT
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `content` TEXT NOT NULL,
+  `image` VARCHAR(255),
+  `treasure_id` INT
 );
 
 CREATE TABLE `Encounter` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `chapter_id` INT,
-    `monster_id` INT
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `chapter_id` INT,
+  `monster_id` INT
 );
 
 CREATE TABLE `Inventory` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `hero_id` INT,
-    `item_id` INT
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `hero_id` INT,
+  `item_id` INT
 );
 
 CREATE TABLE `Links` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `chapter_id` INT,
-    `next_chapter_id` INT,
-    `description` TEXT
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `chapter_id` INT,
+  `next_chapter_id` INT,
+  `description` TEXT
 );
 
 CREATE TABLE `Chapter_Treasure` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `chapter_id` INT,
-    `item_id` INT
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `chapter_id` INT,
+  `item_id` INT
 );
 
 CREATE TABLE `Quest` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `hero_id` INT,
-    `chapter_id` INT
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `hero_id` INT,
+  `chapter_id` INT
 );
 
 CREATE TABLE `User` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `nom` text NOT NULL,
-    `mdp` text NOT NULL,
-    `admin` bool NOT NULL
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nom` text NOT NULL,
+  `mdp` text NOT NULL,
+  `admin` bool NOT NULL
 );
 
 CREATE TABLE `Spell` (
-    `id` int PRIMARY KEY AUTO_INCREMENT,
-    `nom` text,
-    `mana` int NOT NULL
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `nom` text,
+  `mana` int NOT NULL
 );
 
-ALTER TABLE `Spell` ADD FOREIGN KEY (`id`) REFERENCES `Hero` (`spell_id`);
+CREATE TABLE `Weapons` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `item_id` int NOT NULL,
+  `slot` int NOT NULL,
+  `stat_attaque` int NOT NULL,
+  `stat_def` int NOT NULL
+);
+
+ALTER TABLE `Class` ADD FOREIGN KEY (`secondary_weapon_default`) REFERENCES `Weapons` (`id`);
+
+ALTER TABLE `Class` ADD FOREIGN KEY (`primary_weapon_default`) REFERENCES `Weapons` (`id`);
+
+ALTER TABLE `Hero` ADD FOREIGN KEY (`shield`) REFERENCES `Weapons` (`id`);
+
+ALTER TABLE `Hero` ADD FOREIGN KEY (`secondary_weapon`) REFERENCES `Weapons` (`id`);
+
+ALTER TABLE `Hero` ADD FOREIGN KEY (`primary_weapon`) REFERENCES `Weapons` (`id`);
+
+ALTER TABLE `Weapons` ADD FOREIGN KEY (`item_id`) REFERENCES `Items` (`id`);
+
+ALTER TABLE `Hero` ADD FOREIGN KEY (`spell_id`) REFERENCES `Spell` (`id`);
 
 ALTER TABLE `Hero` ADD FOREIGN KEY (`user_id`) REFERENCES `User` (`id`);
 
